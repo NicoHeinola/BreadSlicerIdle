cab29ee10cf5ba38e411d371461ef511528ae16d
Nico Heinola <nico.heinola@gmail.com>
2024-03-19 18:32:38 +0200

    Made money label work

--- scripts/GlobalStats.gd
+++ scripts/GlobalStats.gd
@@ -1,15 +1,16 @@
 extends Node
 
+signal money_changed(new_value: int, old_value: int)
+
 var money: int :
 	get:
 		return money
 	set(value):
-		money_changed.emit()
+		print("New money: ", value)
+		money_changed.emit(value, money)
 		money = value
 	
 var gems: int = 0
 
 var ascension_points: int = 0
 var super_ascension_points: int = 0
-
-signal money_changed()
